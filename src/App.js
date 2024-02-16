a27ada0205aa34d89aa73b8b9abe1405fb00d9c5
sjeevigit <siranjeevijeevi123@gmail.com>
2024-02-16 19:41:01 +0530

    first coimmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import About from "./components/about";
+import Contact from "./components/contact";
+import Navbar from "./components/navbar";
+import Projects from "./components/projects";
+import Skills from "./components/skills";
+import Testimonials from "./components/testimonials";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
   );
 }
-
-export default App;
---
+++ src/data.js
@@ -0,0 +1,52 @@
+export const projects = [
+    {
+      title: "React Reserv",
+      subtitle: "MERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-1.gif",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./pro1.png",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.gif",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+  export const testimonials = [
+    {
+      quote:
+        "Sj is pretty cool I guess. He gives us lots of cat treats.",
+      image: "./port1.jpg",
+      name: "Sanjay",
+      company: "vel tech",
+    },
+    {
+      quote:
+        "Best test soft data",
+      image: "./port2.jpg",
+      name: "praba",
+      company: "Rko test",
+    },
+  ];
+  
+  export const skills = [
+    "JavaScript",
+    "React",
+    "MongoDB",
+    "Node",
+    "SQL",
+    "Express",
+    "Cad"
+  ];
